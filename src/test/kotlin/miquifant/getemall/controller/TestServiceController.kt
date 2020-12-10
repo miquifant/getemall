b6211b3e886ec99be0f883963275e40da91cb896
miquifant <miqui.infante@gmail.com>
2020-12-10 22:24:55 +0100

    fix: fix tests on ServiceController
    An error ocurred when cherrypicking changes for previous PR which caused
    the class not to compile.

--- src/test/kotlin/miquifant/getemall/controller/TestServiceController.kt
+++ src/test/kotlin/miquifant/getemall/controller/TestServiceController.kt
@@ -1,22 +1,29 @@
+/**
+ * Test functions of ServiceController.
+ *
+ * Created by miquifant on 2020-12-09
+ */
 package miquifant.getemall.controller
 
 import miquifant.getemall.api.authentication.User
 import miquifant.getemall.api.authentication.UserDao
 import miquifant.getemall.api.authentication.impl.UserListDao
-import miquifant.getemall.api.controller.ComponentCheck
+import miquifant.getemall.api.controller.ComponentStatus.DOWN
+import miquifant.getemall.api.controller.ComponentStatus.OK
 import miquifant.getemall.api.controller.ServiceController
-import miquifant.getemall.log.Loggable
+import miquifant.getemall.log.Loggable.Logger
 
 import org.junit.Test
 
 import kotlin.test.assertEquals
 import kotlin.test.assertNull
 
+
 class TestServiceController {
 
   // Prepare a UserDao implementation which is never ready, by design
   object UnreadyUserDao: UserDao {
-    override fun authenticate(username: String?, password: String?, accessLogger: Loggable.Logger): User? = null
+    override fun authenticate(username: String?, password: String?, accessLogger: Logger): User? = null
     override fun getUserByUsername(username: String): User? = throw RuntimeException("fail by design")
   }
 
@@ -25,10 +32,10 @@ class TestServiceController {
     val checkReady = ServiceController.checkLogin(UserListDao())
     val checkUnready = ServiceController.checkLogin(UnreadyUserDao)
 
-    assertEquals(ComponentCheck.ComponentStatus.OK, checkReady.status)
+    assertEquals(OK, checkReady.status)
     assertNull(checkReady.message, "Message should be null")
 
-    assertEquals(ComponentCheck.ComponentStatus.DOWN, checkUnready.status)
+    assertEquals(DOWN, checkUnready.status)
     assertEquals("Login component not ready", checkUnready.message)
   }
 }
