03f3d48f67cfcae2aa0187eadba6d4a2b68f3c88
miquifant <miqui.infante@gmail.com>
2020-12-28 03:23:39 +0100

    feat: add bootstrap
    - added jQuery and bootstrap (3.4 with glyphicons) dependencies
    - included in layout and redesigned views
    - changed main.css to getemall.css
    - linked javalin, vuejs, bootstrap, glyphicons,...

--- src/main/resources/vue/views/unauthorized.vue
+++ src/main/resources/vue/views/unauthorized.vue
@@ -1,16 +1,18 @@
-    <template id="unauthorized">
-      <app-frame>
-        <h1 class="title-unauthorized">I'm sorry, {{ $javalin.state.currentUser }}</h1>
-        <h2>As <em>{{ $javalin.state.currentRole }}</em>, you don't have permission to see this page</h2>
-      </app-frame>
-    </template>
-    <script>
-      Vue.component("unauthorized", {
-        template: "#unauthorized"
-      });
-    </script>
-    <style>
-      .title-unauthorized {
-        color: #f50;
-      }
-    </style>
+<template id="unauthorized">
+  <app-frame>
+    <h1 class="title-unauthorized">I'm sorry, {{ $javalin.state.currentUser }}</h1>
+    <h2>As <em>{{ $javalin.state.currentRole }}</em>, you don't have permission to see this page</h2>
+  </app-frame>
+</template>
+
+<script>
+Vue.component("unauthorized", {
+  template: "#unauthorized"
+});
+</script>
+
+<style>
+.title-unauthorized {
+  color: #f50;
+}
+</style>
--- src/main/resources/vue/views/view-1.vue
+++ src/main/resources/vue/views/view-1.vue
@@ -1,16 +1,21 @@
-    <template id="view-1">
-      <app-frame>
-        <h1 class="title-view-1">This is the first view</h1>
-        <h2>Anyone can see it</h2>
-      </app-frame>
-    </template>
-    <script>
-      Vue.component("view-1", {
-        template: "#view-1"
-      });
-    </script>
-    <style>
-      .title-view-1 {
-        color: blue;
-      }
-    </style>
+<template id="view-1">
+  <app-frame>
+    <h1 class="title-view-1">This is the first view</h1>
+    <h2>Anyone can see it</h2>
+  </app-frame>
+</template>
+
+<script>
+  Vue.component("view-1", {
+    template: "#view-1",
+    mounted: function() {
+      $("#v1-tab").tab("show")
+    }
+  });
+</script>
+
+<style>
+.title-view-1 {
+  color: blue;
+}
+</style>
